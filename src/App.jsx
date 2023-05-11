afbb9964826117dbb38397a1477df06498bc85a0
TrueMolni <truemolni@gmail.com>
2023-05-11 13:16:28 +0300

    0.0.1
    added packajes, and base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+
+export const App = () => {
+  return (
+    // <Provider store={store}>
+    // <BrowserRouter>
+    <h1>Test</h1>
+    // </BrowserRouter>
+    // </Provider>
+  );
+};
