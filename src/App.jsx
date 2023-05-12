0298900bd90d4e27d0db2efb7903f69910755737
Mariia Makarkina <mariiamak25@gmail.com>
2023-05-12 15:10:58 +0300

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
+import sprite from '../src/images/icons/sprite.svg';
 
 export const App = () => {
   return (
     // <Provider store={store}>
     // <BrowserRouter>
-    <h1>Test</h1>
+    <>
+      <h1>Test</h1>
+      <svg>
+        <use xlinkHref={`${sprite}#round`} />
+      </svg>
+    </>
     // </BrowserRouter>
     // </Provider>
   );
