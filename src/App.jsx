13783364f7f2cbd4815f66f101155f532f6f97d2
Mariia Makarkina <mariiamak25@gmail.com>
2023-05-12 15:27:33 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 // import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
-import sprite from '../src/images/icons/sprite.svg';
 
 export const App = () => {
   return (
     // <Provider store={store}>
     // <BrowserRouter>
-    <>
-      <h1>Test</h1>
-      <svg>
-        <use xlinkHref={`${sprite}#round`} />
-      </svg>
-    </>
+    <h1>Test</h1>
     // </BrowserRouter>
     // </Provider>
   );
