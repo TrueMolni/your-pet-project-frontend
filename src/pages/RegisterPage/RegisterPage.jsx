94da1431af4dc3d5bc8220a3b002eff141cea5ad
max-ignat <cadillac.maxim@gmail.com>
2023-05-14 14:43:13 +0300

    add two routes for test

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -2,8 +2,7 @@ import { RegisterForm } from "components/AuthForm/RegisterForm";
 const RegisterPage = () => {
   return (
     <div>
-      <h1>This is Register page</h1>
-      <p>Let`s create your accaunt</p>
+      
       <RegisterForm title={("Register")} />
     </div>
   );
