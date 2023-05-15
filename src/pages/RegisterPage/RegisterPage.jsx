e409b7ba6537cf1f771233dbf88d1b40fb357042
max-ignat <cadillac.maxim@gmail.com>
2023-05-15 18:14:41 +0300

    moved some components

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,10 @@
 import { RegisterForm } from 'modules/AuthForm/RegisterForm';
-import Button from 'shared/components/Button/Button';
+
 const RegisterPage = () => {
   return (
     <div>
       <RegisterForm title={'Register'} />
-      <Button />
+      
     </div>
   );
 };
