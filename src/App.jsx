c6a9db5160a349d20b875a44606cd5da406ccd53
Oleh Honchar <ooollleeeg@gmail.com>
2023-05-16 12:10:46 +0300

    add Loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,14 @@ import SharedLayout from 'modules/SharedLayout';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
 import LoginPage from 'pages/LoginPage/LoginPage';
 
-
 export const App = () => {
   return (
     // <Provider store={store}>
     <BrowserRouter basename="/your-pet-project-frontend">
       <Routes>
         <Route path="/" element={<SharedLayout />}></Route>
-        <Route path= '/login' element ={<LoginPage/>}></Route>
-        <Route path='/register' element={<RegisterPage/>}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
       </Routes>
     </BrowserRouter>
     // </Provider >
