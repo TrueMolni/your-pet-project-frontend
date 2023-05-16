b1770d0bf62dc82dc8e94fae1198a1a9ed3d1c26
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-05-16 14:55:00 +0300

    створює функію для вираховування повної кількості років/місяців

--- src/modules/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/modules/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -5,6 +5,21 @@ import styles from './notice-category-item.module.css';
 const NoticeCategoryItem = ({ petInformation }) => {
   const { _id, date, comments, avatarURL, category, location, sex, title } =
     petInformation;
+
+  const countFullYears = date => {
+    const recordСreationDate = moment(date);
+    const currentDate = moment();
+    const fullYears = currentDate.diff(recordСreationDate, 'years');
+    if (!fullYears) {
+      const fullMonths = currentDate.diff(recordСreationDate, 'months');
+      return `${fullMonths} ${
+        fullMonths === 1 || fullMonths === 1 ? 'month' : 'months'
+      }`;
+    }
+    return `${fullYears} ${
+      fullYears === 1 || fullYears === 1 ? 'year' : 'years'
+    }`;
+  };
   return (
     <li key={_id}>
       <div className={styles.mainWrapper}>
@@ -30,7 +45,7 @@ const NoticeCategoryItem = ({ petInformation }) => {
               <svg width="24" height="24">
                 <use xlinkHref={`${sprite}#clock`}></use>
               </svg>
-              <span>{date}</span>
+              <span>{countFullYears()}</span>
             </p>
             <p className={styles.itemDescription}>
               <svg width="24" height="24">
