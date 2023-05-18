725cde71d31e29073f7ec5eb4a25dc113fafb0f8
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-05-18 09:27:46 +0300

    видаляє функії з компонента та імпортує їх з helpersNoticeCategoryItem.js

--- src/modules/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/modules/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,4 +1,4 @@
-import moment from 'moment/moment';
+import { countFullYears, selectIconGender } from 'helpers/noticesHelpers';
 import sprite from '../../images/icons/sprite.svg';
 import styles from './notice-category-item.module.css';
 
@@ -6,27 +6,6 @@ const NoticeCategoryItem = ({ petInformation }) => {
   const { _id, date, comments, avatarURL, category, location, sex, title } =
     petInformation;
 
-  const countFullYears = () => {
-    const recordСreationDate = moment(date);
-    const currentDate = moment();
-    const fullYears = currentDate.diff(recordСreationDate, 'years');
-    if (!fullYears) {
-      const fullMonths = currentDate.diff(recordСreationDate, 'months');
-      return `${fullMonths} ${
-        fullMonths === 1 || fullMonths === 1 ? 'month' : 'months'
-      }`;
-    }
-    return `${fullYears} ${
-      fullYears === 1 || fullYears === 1 ? 'year' : 'years'
-    }`;
-  };
-
-  const selectIconGender = () => {
-    if (sex === 'male') {
-      return `${sprite}#male`;
-    }
-    return `${sprite}#female`;
-  };
   return (
     <li key={_id}>
       <div className={styles.mainWrapper}>
@@ -61,11 +40,11 @@ const NoticeCategoryItem = ({ petInformation }) => {
               <svg width="24" height="24">
                 <use xlinkHref={`${sprite}#clock`}></use>
               </svg>
-              <span>{countFullYears()}</span>
+              <span>{countFullYears(date)}</span>
             </p>
             <p className={styles.itemDescription}>
               <svg width="24" height="24">
-                <use xlinkHref={selectIconGender()}></use>
+                <use xlinkHref={selectIconGender(sex)}></use>
               </svg>
               <span>{sex}</span>
             </p>
