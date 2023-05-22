3b4e52048ebc2e74a0025abca20478a875c58a33
max-ignat <cadillac.maxim@gmail.com>
2023-05-22 21:29:17 +0300

    small fix with registration

--- src/modules/Navigation/UserNavigation/UserNavigation.jsx
+++ src/modules/Navigation/UserNavigation/UserNavigation.jsx
@@ -1,52 +1,19 @@
-// import { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-// import { NavLink } from 'react-router-dom';
-// import { getUserEmail, } from 'redux/auth/auth-selectors';
-// import sprite from '../../../images/icons/sprite.svg';
-
-// const UserNavigation = ({ userNameStyle, userIconContainer }) => {
-//   const userEmail = useSelector(getUserEmail);
-
-//   const [name, setName] = useState(userEmail || 'user');
-
-//   useEffect(() => {
-//     setName(userEmail || 'user');
-//   }, [userEmail]);
-
-//   return (
-//     <NavLink to={'/user'} className={userIconContainer}>
-//       <svg width={28} height={28}>
-//         <use href={sprite + '#user'}></use>
-//       </svg>
-//       <p className={userNameStyle}>{name}</p>
-//     </NavLink>
-//   );
-// };
-// export default UserNavigation;
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-// import { getUser } from 'redux/auth/auth-selectors';
-import { getUserEmail } from 'redux/auth/auth-selectors';
+import { getUser } from 'redux/auth/auth-selectors';
 import sprite from '../../../images/icons/sprite.svg';
 
 const UserNavigation = ({ userNameStyle, userIconContainer }) => {
-  const [name, setName] = useState('user');
-  const userEmail = useSelector(getUserEmail);
-
-  // const user = useSelector(getUser);
+  const [name, setName] = useState('');
+  const user = useSelector(getUser);
   useEffect(() => {
-    // const infoUser = () => {
-    //   if (!!user) return setName(user);
-    //   return setName('user');
-    // };
-    // infoUser();
     const infoUser = () => {
-      if (userEmail) return setName(userEmail);
-      return;
+      if (!!user) return setName(user);
+      return setName('user');
     };
     infoUser();
-  }, [userEmail]);
+  }, [name, user]);
 
   return (
     <NavLink to={'/user'} className={userIconContainer}>
