5e8509a959a59726bd3c1cd8945f888db898a43e
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-05-26 18:48:49 +0200

    addpet

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,19 +1,51 @@
-import React, {
-
-} from 'react';
-import css from './UserPage.module.css';
+// import ButtonNav from "shared/components/Button/ButtonNav";
+// import { useDispatch } from "react-redux";
+// import { logout } from "redux/auth/auth-operations";
+import React, { useEffect } from 'react';
+// import { addUserInfo } from '../../redux/userInfo/user-operations';
+// import Button from 'shared/components/Button';
+import styles from './UserPage.module.css';
 import UserData from '../../modules/UserData/UserData.jsx';
 import Logout from '../../modules/Logout/Logout.jsx';
-import { useDispatch } from "react-redux";
-import { logout } from 'redux/auth/auth-operations';
+// import axios from 'axios';
+
+import PetsData from 'modules/PetsData/PetsData';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, openModal } from 'redux/auth/auth-operations';
+
+import ModalCongrats from 'modules/ModalCongrats/ModalCongrats';
+import { isModalOpen } from 'redux/auth/auth-selectors';
+
 const UserPage = () => {
+  const showModal = useSelector(isModalOpen);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!showModal) {
+      const timer = setTimeout(() => {
+        dispatch(openModal());
+      });
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [dispatch, showModal]);
+
   return (
-<section className={css.container}>
-      <UserData />
-      <Logout onClick={() => dispatch(logout())} />
-      {/* <Logout /> */}
-</section>
+    <>
+      {showModal && <ModalCongrats />}
+      <section className={styles.container}>
+        <div className={styles.userWrap}>
+          <div className={styles.userDataBox}>
+            <UserData />
+            <Logout onClick={() => dispatch(logout())} />
+          </div>
+          <div className={styles.petsDataBox}>
+            <PetsData />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 export default UserPage;
