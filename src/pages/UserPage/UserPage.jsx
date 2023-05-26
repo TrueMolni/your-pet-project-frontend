5ee2d7323e1f3fc24b19556b5eabb0b27f141db3
Oleh Honchar <ooollleeeg@gmail.com>
2023-05-26 14:48:39 +0300

    userPage стилізація

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,80 +1,54 @@
 // import ButtonNav from "shared/components/Button/ButtonNav";
 // import { useDispatch } from "react-redux";
 // import { logout } from "redux/auth/auth-operations";
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 // import { addUserInfo } from '../../redux/userInfo/user-operations';
 // import Button from 'shared/components/Button';
 import styles from './UserPage.module.css';
 import UserData from '../../modules/UserData/UserData.jsx';
 import Logout from '../../modules/Logout/Logout.jsx';
 // import axios from 'axios';
-import ButtonNav from 'shared/components/Button/ButtonNav';
+
 import PetsData from 'modules/PetsData/PetsData';
-import { useDispatch , useSelector} from 'react-redux';
-import { logout , openModal} from 'redux/auth/auth-operations';
-import css from '../../modules/Navigation/AuthNavigation/auth-navigation.module.css';
-import ModalCongrats from "modules/ModalCongrats/ModalCongrats";
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, openModal } from 'redux/auth/auth-operations';
+
+import ModalCongrats from 'modules/ModalCongrats/ModalCongrats';
 import { isModalOpen } from 'redux/auth/auth-selectors';
 
 const UserPage = () => {
   const showModal = useSelector(isModalOpen);
   const dispatch = useDispatch();
-  
 
   useEffect(() => {
     if (!showModal) {
-    const timer = setTimeout(() => {
-      dispatch(openModal());
-    }, );
-    return () => {
-      clearTimeout(timer);
-    };
-  }
-  
-}, [dispatch, showModal]);
+      const timer = setTimeout(() => {
+        dispatch(openModal());
+      });
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [dispatch, showModal]);
 
-  
   return (
     <>
       {showModal && <ModalCongrats />}
       <section className={styles.container}>
-        <div
-          style={{
-            fontWeight: 800,
-            fontSize: 20,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <UserData />
-          <Logout onClick={() => dispatch(logout())} />
-
-          <ButtonNav
-            customStyle={css.loginBtn}
-            buttonName={'Logout'}
-            type="button"
-            onClick={() => dispatch(logout())}
-          />
-          <PetsData />
+        <div className={styles.userWrap}>
+          <div className={styles.userDataBox}>
+            <UserData />
+            <Logout onClick={() => dispatch(logout())} />
+          </div>
+          <div className={styles.petsDataBox}>
+            <PetsData />
+          </div>
         </div>
       </section>
     </>
   );
 };
 export default UserPage;
-
-
-
-
-
-
-
-
-
-
-
 
 // const UserPages = () => {
 
